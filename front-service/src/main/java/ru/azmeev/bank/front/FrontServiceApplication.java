da1190c80c85e8a5d86a9340d221780f279043ac
A.Zmeev <i@azmeev.ru>
2025-05-15 15:09:20 +0300

    Add front service module

---
+++ front-service/src/main/java/ru/azmeev/bank/front/FrontServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.azmeev.bank.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontServiceApplication.class, args);
+    }
+
+}
