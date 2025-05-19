652a91c9c2f06c26cc9b183c5e050f30d8b4f543
A.Zmeev <i@azmeev.ru>
2025-05-19 17:02:33 +0300

    Add blocker service

---
+++ notification-service/src/main/java/ru/azmeev/bank/notification/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.azmeev.bank.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
